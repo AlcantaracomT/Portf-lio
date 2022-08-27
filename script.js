29784d01f6ebde58c3f79d090c27923a2e268284
Thiago Alcantara <alcantara.ta2213@gmail.com>
2022-08-27 19:08:21 -0300

    deleted img, added button and about

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var containerHtml = document.querySelector('.animacao')
 
 btn.addEventListener('click', function() {
 	if(containerHtml.style.display === 'none') {
-    containerHtml.style.display = 'flex';
+        containerHtml.style.display = 'flex';
 } else {
 	  containerHtml.style.display = 'none';
 }
@@ -16,7 +16,7 @@ var containerCss = document.querySelector('.animacao1')
 
 btn.addEventListener('click', function() {
 	if(containerCss.style.display === 'none') {
-   	containerCss.style.display = 'flex';
+   	    containerCss.style.display = 'flex';
 } else {
 	  containerCss.style.display = 'none';
 }
@@ -28,7 +28,7 @@ var containerJs = document.querySelector('.animacao2')
 
 btn.addEventListener('click', function() {
 	if(containerJs.style.display === 'none') {
-   	containerJs.style.display = 'flex';
+   	    containerJs.style.display = 'flex';
 } else {
 	  containerJs.style.display = 'none';
 }
