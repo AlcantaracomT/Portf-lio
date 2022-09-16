97d9c5fa76c942212a546d6f5d7b19ba2c9a58b5
Thiago Alcantara <alcantara.ta2213@gmail.com>
2022-09-16 19:17:13 -0300

    testando js

--- src/script.js
+++ src/script.js
@@ -1,33 +1,10 @@
-const controls = document.querySelectorAll(".control");
-let currentItem = 0;
-const items = document.querySelectorAll(".item");
-const maxItems = items.length;
+const bnt = document.querySelector('#butto');
+const animacao = document.querySelector('.animacao');
 
-controls.forEach((control) => {
-  control.addEventListener("click", (e) => {
-    isLeft = e.target.classList.contains("arrow-left");
-
-    if (isLeft) {
-      currentItem -= 1;
-    } else {
-      currentItem += 1;
-    }
-
-    if (currentItem >= maxItems) {
-      currentItem = 0;
-    }
-
-    if (currentItem < 0) {
-      currentItem = maxItems - 1;
-    }
-
-    items.forEach((item) => item.classList.remove("current-item"));
-
-    items[currentItem].scrollIntoView({
-      behavior: "smooth",
-      inline: "center"
-    });
-
-    items[currentItem].classList.add("current-item");
-  });
+bnt.addEventListener("click", function() {
+	if (animacao.style.display === "none") {
+		animacao.style.display = "flex";
+	} else {
+		animacao.style.display = "none";
+	}
 });
