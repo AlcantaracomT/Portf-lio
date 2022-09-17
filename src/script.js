9aab44ab315e6852ae64068ffa246ee3834967df
AlcantaracomT <98988893+AlcantaracomT@users.noreply.github.com>
2022-09-17 17:46:14 -0300

    Added javaScript

--- src/script.js
+++ src/script.js
@@ -1,10 +1,32 @@
-const bnt = document.querySelector('#butto');
-const animacao = document.querySelector('.animacao');
+const butHtml = document.querySelector("#butHtml");
+const animacao = document.querySelector(".buttonH");
 
-bnt.addEventListener("click", function() {
+butHtml.addEventListener("click", function() {
 	if (animacao.style.display === "none") {
 		animacao.style.display = "flex";
 	} else {
 		animacao.style.display = "none";
+	}
+});
+
+const butCss = document.querySelector('#butCss');
+const animacao1 = document.querySelector('.buttonC');
+
+butCss.addEventListener("click", function() {
+	if (animacao1.style.display === "none") {
+		animacao1.style.display = "flex";
+	} else {
+		animacao1.style.display = "none";
+	}
+});
+
+const butJs = document.querySelector('#butJs');
+const animacao2 = document.querySelector('.buttonJ');
+
+butJs.addEventListener("click", function() {
+	if (animacao2.style.display === "none") {
+		animacao2.style.display = "flex";
+	} else {
+		animacao2.style.display = "none";
 	}
 });
